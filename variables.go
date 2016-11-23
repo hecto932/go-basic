5d0b0cf89536ca34a0c4095efb87139ccb79e22c
Hector Flores <hecto932@gmail.com>
2016-11-23 01:04:55 -0600

    Println, ejemplos de datos, variables

---
+++ variables.go
@@ -0,0 +1,23 @@
+// Variables 
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string = "string"
+	fmt.Println(a)
+
+	var b, c, h int = 4, 5, 0
+	fmt.Println(b, c, h)
+
+	var d = false
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f:= "short"
+	fmt.Println(f)
+}
